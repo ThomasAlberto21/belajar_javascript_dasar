e1f8fcb854bf097e78a52d132df5d9ec7e56feb7
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-12 11:45:57 +0700

    Belajar Comments Di JavaScript

---
+++ Comments/comments.js
@@ -0,0 +1,9 @@
+// Todo Komentar Satu Baris
+// Ini merupakan komentar satu baris.
+
+// Todo Komentar Lebih Dari Satu Baris
+/* Ini merupakan komentar dengan lebih dari satu baris
+Teks apapun yang berada disini akan dijadikan komentar.
+Ketika menggunakan ini, jangan lupa untuk menutupnya.
+*/
+
