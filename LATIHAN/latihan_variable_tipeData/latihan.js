b0173f1432a85970ab30a8b8b329209ea395aa9e
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-12 23:02:30 +0700

    Latihan Materi Javascript

---
+++ LATIHAN/latihan_variable_tipeData/latihan.js
@@ -0,0 +1,21 @@
+/*
+
+    * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+    *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+    *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+    *  - age: bertipe data number, dengan nilai umur Anda.
+    *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+
+*/
+
+
+
+firstName = 'Thomas';
+lastName = 'Alberto';
+age = 18;
+isMarried = false;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
