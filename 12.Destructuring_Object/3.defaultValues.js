6735d454fef04327960b0bee8835a455dd2e35af
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-14 21:33:42 +0700

    Belajar Destructuring Object Di Javascript

--- 12.Destructuring_Object/3.defaultValues.js
+++ 12.Destructuring_Object/3.defaultValues.js
@@ -7,20 +7,42 @@
 
 */
 
-
-
-// Contoh 1
+// Sebelum menambahkan default values
 const profile = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 18
+}
+ 
+ 
+const { firstName, age, isMale } = profile;
+ 
+console.log(firstName)
+console.log(age)
+console.log(isMale)
+ 
+/* output:
+John
+18
+undefined
+*/
+
+
+
+
+
+// Sesudah Menambahkan Default Values
+const profile2 = {
   firstName: 'John',
   lastName: 'Doe',
   age: 18,
 };
 
-const { firstName, age, isMale = true } = profile; // Menambahkan nilai default langsung
+const { firstName2, age2, isMale2 = true } = profile; // Menambahkan nilai default langsung
 
-console.log(firstName);
-console.log(age);
-console.log(isMale);
+console.log(firstName2);
+console.log(age2);
+console.log(isMale2);
 
 /* output:
   John
