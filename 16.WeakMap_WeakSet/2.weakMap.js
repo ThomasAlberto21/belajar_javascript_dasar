ae1618ac9520218e3776332522930133667b5939
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-14 22:38:09 +0700

    Belajar Weak Map Dan Weak Set Di Javascript

---
+++ 16.WeakMap_WeakSet/2.weakMap.js
@@ -0,0 +1,42 @@
+// Todo Weak Map
+/*
+
+    * WeakMap merupakan varian dari Map yang mendukung garbage collection. Garbage collection adalah proses di mana interpreter JavaScript mengambil kembali memori yang tidak lagi “dapat dijangkau” dan tidak dapat digunakan oleh program [3]. Garbage collection di JavaScript dilakukan secara otomatis dan bukan menjadi urusan dari developer.
+
+    * Yang dimaksud weak dalam WeakMap adalah referensi terhadap nilai yang disimpan. Apabila suatu nilai yang disimpan di WeakMap sudah tidak terjangkau atau tidak bisa lagi diakses, maka referensi ke memorinya akan dihapus.
+
+    * Berikut ini adalah beberapa hal yang membedakan antara Map dan WeakMap:
+
+    ? Key dari WeakMap harus berupa object atau array. Nilai primitif tidak bisa digunakan sebagai key karena tidak mendukung garbage collection.
+
+    ? WeakMap memiliki method get(), set(), has(), dan delete(). Namun, WeakMap tidak termasuk kategori iterable sehingga tidak memiliki method keys(), values(), atau forEach().
+
+    ? WeakMap juga tidak memiliki property size. Ini karena ukuran WeakMap dapat berubah karena proses garbage collection.
+
+*/
+
+
+
+// Contoh
+const { inspect } = require('util');
+
+const visitsCountMap = new WeakMap(); // Menyimpan daftar user
+
+function countUser(user) {
+  let count = visitsCountMap.get(user) || 0;
+  visitsCountMap.set(user, count + 1);
+}
+
+let jonas = { name: "Jonas" };
+countUser(jonas);  // Menambahkan user "Jonas"
+
+jonas = null;  // Data object "Jonas" dihapus
+
+// delay dibutuhkan untuk menunggu garbage collector bekerja
+setTimeout(function() {
+  console.log(inspect(visitsCountMap, { showHidden: true }));
+}, 4000);
+
+/* output
+  WeakMap {  }
+*/
---
+++ 16.WeakMap_WeakSet/3.weakSet.js
@@ -0,0 +1,8 @@
+// Todo Weak Set
+/*
+
+    * WeakSet tidak bisa menyimpan nilai primitif.
+    * WeakSet bukan iterable dan hanya memiliki method add(), has(), dan delete().
+    * WeakSet tidak memiliki properti size.
+
+*/
